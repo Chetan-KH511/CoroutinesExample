6cc18d7f4842a896c4ffa8bf084acb58fbd885c0
Chetan Bhat <chetanbhatmb@gmail.com>
2024-01-15 16:03:29 +0530

    Add withContext() to switch between threads

--- app/src/main/java/com/mastercoding/coroutines/MainActivity.kt
+++ app/src/main/java/com/mastercoding/coroutines/MainActivity.kt
@@ -9,8 +9,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
 
     lateinit var binding: ActivityMainBinding
 
@@ -45,9 +47,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun downloadBigfileFromNet() {
+    private  suspend fun downloadBigfileFromNet() {
         for(i in 1..100000){
             //Log.i("TAGY", "Downloading $i in ${Thread.currentThread().name}")
+
+
+            //Here since we are tryinng to change the view made in main thread , we need to switch our thread from worker(IO) to main i.e use withContext()
+
+
+            withContext(Dispatchers.Main){
+                binding.downloadProgress.text = "$i in ${Thread.currentThread().name}"
+
+            }
 
         }
     }
